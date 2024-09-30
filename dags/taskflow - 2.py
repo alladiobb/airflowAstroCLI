1d4e3b0259845ca85c2d1a23f229a4185ec626be
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-09-30 19:50:20 -0300

    Add: taskflow - 2

---
+++ dags/taskflow - 2.py
@@ -0,0 +1,33 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+def _task_a():
+    print("Task A")
+    return 42
+
+def _task_b():
+    print("Task B")
+    print(ti.xcom_pull(task_ids='task_a'))
+
+@dag(
+    start_date=datetime(2024,9,30),
+    schedule='@daily',
+    catchup=False,
+    tags=['taskflow']
+)
+
+def taskflow():
+    task_a = PythonOperator(
+        task_id='task_a',
+        python_callable=_task_a,
+
+    )
+    task_b = PythonOperator(
+        task_id='task_b',
+        python_callable=_task_b,
+
+    )
+
+    task_a >> task_b
+
+taskflow()
---
+++ dags/taskflow - 3.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.decorators.python import PythonOperator
+from datetime import datetime
+
+def _task_a():
+    print("Task A")
+    return 42
+
+def _task_b():
+    print("Task B")
+    print(ti.xcom_pull(task_ids='task_a'))
+
+with DAG(
+    dag_id='taskflow',
+    start_date=datetime(2024,9,30),
+    schedule='@daily',
+    catchup=False,
+    tags=['taskflow']
+):
+    
+    task_a = PythonOperator(
+        task_id='task_a',
+        python_callable=_task_a,
+
+    )
+    task_b = PythonOperator(
+        task_id='task_b',
+        python_callable=_task_b,
+
+    )
+
+    task_a >> task_b
